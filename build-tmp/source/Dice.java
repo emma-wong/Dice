9eeae288f3adae32cb8e6455327fc46e9a62ab05
emma-wong <emmawongsf@gmail.com>
2015-09-11 11:04:43 -0700

    Change code for dice

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,12 +1,27 @@
-//initialize var for total
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 int sum = 0;
 
-void setup() {
+public void setup() {
     size(800, 800);
     noLoop();
 }
 
-void draw() {
+public void draw() {
     background(0, 0, 0);
     for (int y = 10; y <= 730; y += 70) {
         for (int i = 10; i <= 780; i += 66) {
@@ -20,11 +35,10 @@ void draw() {
     text("Total: " + sum, 300, 785);
 }
 
-void mousePressed() {
+public void mousePressed() {
     redraw();
     sum = 0;
 }
-
 class Die //models one single dice cube
 {
     int roll;
@@ -37,7 +51,7 @@ class Die //models one single dice cube
             myY = y;
         }
 
-    void roll() {
+    public void roll() {
         if (roll == 1) {
             fill((int)(Math.random() * 255), (int)(Math.random() * 255), (int)(Math.random() * 255));
             ellipse(myX + 25, myY + 25, 10, 10);
@@ -74,10 +88,19 @@ class Die //models one single dice cube
         }
     }
 
-    void show() {
+    public void show() {
         stroke(2);
         fill(255, 255, 255);
         rect(myX, myY, 50, 50, 7);
         textSize(20);
     }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
